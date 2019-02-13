41cdc3e761f26d22b83406e61d65c85fcd26e0c1
celloworld <camerondonahue@gmail.com>
2019-02-13 12:22:41 -0800

    Initialized angular app, added 2 components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { ChatItemComponent } from './chat-item/chat-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputComponent,
+    ChatItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/chat-item/chat-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-item',
+  templateUrl: './chat-item.component.html',
+  styleUrls: ['./chat-item.component.sass']
+})
+export class ChatItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
